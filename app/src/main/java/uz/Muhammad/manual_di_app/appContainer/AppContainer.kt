7ca9edd26bb45c7d036fcd06f3ca7645793c926c
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-18 16:20:15 +0500

    Retrofit, logging, http client, moshi added

---
+++ app/src/main/java/uz/Muhammad/manual_di_app/appContainer/AppContainer.kt
@@ -0,0 +1,31 @@
+package uz.Muhammad.manual_di_app.appContainer
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import uz.Muhammad.manual_di_app.BuildConfig
+import kotlin.apply
+
+class AppContainer {
+    private val logging = HttpLoggingInterceptor().apply{
+        level = if(BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+        else HttpLoggingInterceptor.Level.NONE
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://dummyjson.com")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(okHttpClient)
+        .build()
+}
