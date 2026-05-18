2ed65dace86d9d87d424dc6e737c61f6981a2a10
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-18 17:16:59 +0500

    navigation added to activity

--- app/src/main/java/uz/Muhammad/manual_di_app/MainActivity.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import uz.Muhammad.manual_di_app.navigation.AppNavigation
 import uz.Muhammad.manual_di_app.ui.home.viewmodel.HomeViewModelFactory
 import uz.Muhammad.manual_di_app.ui.theme.ManualDIappTheme
 
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ManualDIappTheme {
+                AppNavigation(homeViewModelFactory)
             }
         }
     }
