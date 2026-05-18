9730d176b4ea35e385da5473d96a60d641331c48
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-18 16:35:58 +0500

    Repository created

--- app/src/main/java/uz/Muhammad/manual_di_app/data/repository/posts/PostsRepository.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/data/repository/posts/PostsRepository.kt
@@ -1,5 +1,22 @@
 package uz.Muhammad.manual_di_app.data.repository.posts
 
-class PostsRepository{
+import uz.Muhammad.manual_di_app.data.remote.api.ApiService
+import uz.Muhammad.manual_di_app.data.remote.dto.posts
 
+class PostsRepository(
+    private val api: ApiService
+){
+    suspend fun getPosts(): Result<posts>{
+        return try {
+            val response = api.getPosts()
+            val body = response.body()
+            if(body!=null){
+                Result.success(body)
+            }else{
+                Result.failure(Exception("Empty response"))
+            }
+        } catch (e: Exception){
+            Result.failure(e)
+        }
+    }
 }
