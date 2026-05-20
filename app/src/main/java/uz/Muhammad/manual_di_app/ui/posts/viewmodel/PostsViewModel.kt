4f3f3de4523149f33d6da30e51fcffd1d8ffaaea
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-20 16:52:47 +0500

    fetching and saving all posts

--- app/src/main/java/uz/Muhammad/manual_di_app/ui/posts/viewmodel/PostsViewModel.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/ui/posts/viewmodel/PostsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import uz.Muhammad.manual_di_app.data.remote.dto.posts.Post
 import uz.Muhammad.manual_di_app.data.repository.posts.PostsRepository
 
 class PostsViewModel(
@@ -14,11 +15,22 @@ class PostsViewModel(
     private val _post = MutableStateFlow<String?>(null)
     val post: StateFlow<String?> = _post
 
+    private val _allPosts = mutableListOf<Post>()
+    private var currentIndex = 0
+
+    fun nextPost(){
+        if(currentIndex< _allPosts.size - 1){
+            currentIndex++
+            _post.value = _allPosts[currentIndex].body
+        }
+    }
+
     fun getPost(){
         viewModelScope.launch {
             val result = repository.getPosts()
             result .onSuccess{
-                _post.value = it.posts[0].body
+                _allPosts.addAll(it.posts)
+                _post.value = it.posts[currentIndex].body
             }
         }
     }
