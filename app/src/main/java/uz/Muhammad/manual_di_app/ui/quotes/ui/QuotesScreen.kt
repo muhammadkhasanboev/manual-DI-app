7b1d1f428c3fbf8b38a772dfa43b0f0b250c8a79
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-20 16:59:18 +0500

    next quote function and button added

--- app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/ui/QuotesScreen.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/ui/QuotesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,6 +34,12 @@ fun QuotesScreen(
         when {
                 quote != null -> Text("$quote")
             else -> Text("Network problems")
+        }
+
+        Button(
+            onClick = {viewModel.nextQuote()}
+        ){
+            Text("Next quote")
         }
     }
 }
--- app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import uz.Muhammad.manual_di_app.data.remote.dto.quotes.Quote
 import uz.Muhammad.manual_di_app.data.repository.quotes.QuotesRepository
 
 class QuotesViewModel(
@@ -13,11 +14,21 @@ class QuotesViewModel(
     private val _quote = MutableStateFlow<String?>(null)
     val quote : StateFlow<String?> = _quote
 
+    private val _allQuotes = mutableListOf<Quote>()
+    private var currentIndex = 0
+
+    fun nextQuote(){
+        if(currentIndex<_allQuotes.size-1){
+            currentIndex++
+            _quote.value = _allQuotes[currentIndex].quote
+        }
+    }
+
     fun getQuote(){
         viewModelScope.launch {
             val result = repository.getQuotes()
             result.onSuccess {
-                _quote.value = it.quotes[0].quote
+                _quote.value = it.quotes[currentIndex].quote
             }
         }
     }
