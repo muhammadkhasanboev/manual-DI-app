cf5f1ed6751b708a4983a3ab1a25998f3bcfb945
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-20 13:45:11 +0500

    new endpoint added

--- app/src/main/java/uz/Muhammad/manual_di_app/data/remote/api/ApiService.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/data/remote/api/ApiService.kt
@@ -3,8 +3,12 @@ package uz.Muhammad.manual_di_app.data.remote.api
 import retrofit2.Response
 import retrofit2.http.GET
 import uz.Muhammad.manual_di_app.data.remote.dto.posts.posts
+import uz.Muhammad.manual_di_app.data.remote.dto.quotes.Quotes
 
 interface ApiService {
     @GET("/posts")
     suspend fun getPosts(): Response<posts>
+
+    @GET("/quotes")
+    suspend fun getQuotes(): Response<Quotes>
 }
