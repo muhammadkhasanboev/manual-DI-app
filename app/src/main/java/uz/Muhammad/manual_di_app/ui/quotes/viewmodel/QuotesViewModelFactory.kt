9d262973ac181e5fe8898ae4a2f9390a88c4872b
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-20 16:27:42 +0500

    quotes viewmodel factory added

--- app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModelFactory.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModelFactory.kt
@@ -1,4 +1,18 @@
 package uz.Muhammad.manual_di_app.ui.quotes.viewmodel
 
-class QuotesViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import uz.Muhammad.manual_di_app.data.repository.quotes.QuotesRepository
+import uz.Muhammad.manual_di_app.ui.posts.viewmodel.PostsViewModel
+
+class QuotesViewModelFactory(
+    private val repository: QuotesRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(QuotesViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return QuotesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
