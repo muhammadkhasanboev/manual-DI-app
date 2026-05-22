40492108c6313c1aa7867cf287b90e76c4cb44a2
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-22 15:45:23 +0500

    quotes viewmodel flow fixed

--- app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/uz/Muhammad/manual_di_app/ui/quotes/viewmodel/QuotesViewModel.kt
@@ -12,26 +12,31 @@ import uz.Muhammad.manual_di_app.data.repository.quotes.QuotesRepositoryImpl
 class QuotesViewModel(
     private val repository: QuotesRepository
 ): ViewModel() {
-    private val _quote = MutableStateFlow<String?>(null)
-    val quote : StateFlow<String?> = _quote
 
     private val _allQuotes = mutableListOf<Quote>()
     private var currentIndex = 0
 
+    private val _uiState = MutableStateFlow<QuoteUIState>(QuoteUIState.Idle)
+    val uiState: StateFlow<QuoteUIState> = _uiState
+
     fun nextQuote(){
         if(currentIndex<_allQuotes.size-1){
             currentIndex++
-            _quote.value = _allQuotes[currentIndex].quote
+            _uiState.value = QuoteUIState.Success(_allQuotes[currentIndex].quote)
         }
     }
 
     fun getQuote(){
+        _uiState.value = QuoteUIState.Loading
         viewModelScope.launch {
             val result = repository.getQuotes()
             result.onSuccess {
                 _allQuotes.addAll(it.quotes)
-                _quote.value = it.quotes[currentIndex].quote
+                _uiState.value = QuoteUIState.Success(_allQuotes[currentIndex].quote)
             }
+                .onFailure{
+                    _uiState.value = QuoteUIState.Error("Unknown error")
+                }
         }
     }
 }
@@ -40,5 +45,6 @@ class QuotesViewModel(
 sealed class QuoteUIState{
     object Idle: QuoteUIState()
     object Loading: QuoteUIState()
-    object Error: QuoteUIState()
+    data class Error(val message: String): QuoteUIState()
+    data class Success(val quote: String): QuoteUIState()
 }
